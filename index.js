cf76d76d2f85a41e808e036dbd7e7fc5fd78e0e6
renantatsuo <tatsuorenan@gmail.com>
2022-07-11 13:06:49 -0300

    Create app main file

---
+++ index.js
@@ -0,0 +1,30 @@
+import { parseArgs } from "./helpers.js";
+import { onDiff } from "./onDiff.js";
+import { run } from "./run.js";
+
+const ARGS = parseArgs(
+  [
+    {
+      name: "password",
+      options: ["-p", "--password"],
+      required: true,
+      env: process.env.SWAG_PASSWORD,
+    },
+    {
+      name: "url",
+      options: ["-u", "--url"],
+      required: true,
+      env: process.env.SWAG_URL,
+    },
+    {
+      name: "interval",
+      options: ["-i", "--interval"],
+      required: false,
+      env: process.env.CHECK_INTERVAL,
+      default: 10,
+    },
+  ],
+  process.argv.slice(2)
+);
+
+run(ARGS, onDiff);
---
+++ onDiff.js
@@ -0,0 +1,8 @@
+import("colors");
+
+/**
+ * @param {import("diff").ArrayChange[]} diff
+ */
+export const onDiff = async (diff) => {
+  //do whatever you want
+};
